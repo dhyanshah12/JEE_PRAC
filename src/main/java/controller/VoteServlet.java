2fecf93ea3869d02e0ccf21327322090e319a115
dhyanshah12 <dhyanshah70@gmail.com>
2026-09-03 16:16:54 +0530

    validation

--- src/main/java/controller/VoteServlet.java
+++ src/main/java/controller/VoteServlet.java
@@ -19,60 +19,58 @@ public class VoteServlet extends HttpServlet
 		String name = req.getParameter("name");
 		String birthYear = req.getParameter("birthyear");
 		String gender = req.getParameter("gender");
-		String error ="";
+		String error = "";
 		Boolean isError = false;
-		String voteStatus="";
-		String alphaRegEx = "[a-zA-Z]";
+		String voteStatus = "";
+		String alphaRegEx = "[a-zA-Z]+";
 		String digitRegEx = "[0-9]{4}";
 		
 		if(name==null || name.isBlank()) {
 			error = "Please Enter Name<br>";
 			isError = true;
 		}else if(name.matches(alphaRegEx)==false) {
-			error = "Please Enter Valid Name<br>";
+			error += "Please Enter Valid Name<br>";
 			isError = true;
 		}
-		 if(birthYear==null || birthYear.isBlank()) {
-			error += "Please Enter BithYear<br>";
+		if(birthYear==null  || birthYear.isBlank()) {
+			error += "Please Enter BirthYear<br>";
 			isError = true;
 		}else if(birthYear.matches(digitRegEx)==false) {
-			error += "Please Enter Valid Birthyear<br>";
+			error += "Please Enter Valid BirthYear<br>";
 			isError = true;
 		}
-		 if(gender==null || gender.isBlank()) {
-			error += "Please Enter gender";
+		if(gender==null || gender.isBlank()) {
+			error += "Please Enter Gender";
 			isError = true;
 		}
-		 	
-		 	res.setContentType("text/html");
-			//To print on the Browser
-			PrintWriter out = res.getWriter();
+		
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		
+		if(isError==true) {
+			out.print("<font color=red>"+error+"</font>");
+		}else {
+			Calendar c = Calendar.getInstance();
 			
-			if(isError==true) {
-				out.print("<font color=red>"+error+"</font>");
-			}
-			else {
-				Calendar c = Calendar.getInstance();
-				
-				int year = c.get(Calendar.YEAR);
-				int birthyear = Integer.parseInt(birthYear);
-				int age = year-birthyear;
-				
-				if(gender.equals("male") && age>=21) {
-					voteStatus = "Eligible for Vote";
-				}
-				else if(gender.equals("female") && age>=23) {
-					voteStatus = "Eligible for Vote";
-				}else {
-					voteStatus = "Not Eligiblr For Vote";
-				}
+			int year = c.get(Calendar.YEAR);
+			int birthyear = Integer.parseInt(birthYear);
+			int age = year - birthyear;
+			
+			
 
-				out.print("Name => "+name+"<br>");
-				out.print("BirthYear => "+birthYear+"<br>");
-				out.print("Gender => "+gender+"<br>");
-				out.print("VoteStatus => "+voteStatus);
+			if(gender.equals("male") && age>=21) {
+				voteStatus = "Eligible for Vote";
+			}else if(gender.equals("female") && age>=23) {
+				voteStatus= "Eligible For Vote";
+			}else {
+				voteStatus ="Not Eligible For Vote";
 			}
-			System.out.println("VotServlet Called......");
+			out.print("Name => "+name+"<br>");
+			out.print("BithYear => "+birthyear+"<br>");
+			out.print("Gender => "+gender+"<br>");
+			out.print("VoteStatus => "+voteStatus);
+		}		
+		System.out.println("VoteServlet Called.....");
 	}
 	
 }
